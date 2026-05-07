3bbd55d3f20c2d077e27a5023ffe613f7085d1a7
upsalesvideo <ant_bg@mail.ru>
2026-05-07 20:21:56 +0300

    Deploy to wb.bogatushin.ru as standalone site

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,6 @@
 import sys
 import os
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.exceptions import NotFound
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app import app
-
-app.config['APPLICATION_ROOT'] = '/wb'
-app.config['PREFERRED_URL_SCHEME'] = 'https'
-
-application = DispatcherMiddleware(NotFound(), {'/wb': app})
+from app import app as application
